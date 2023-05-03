9891bdbcbbb74b18ae024b1f25336b7f81de1f42
Nate Chodosh <nchodosh@andrew.cmu.edu>
2023-05-03 15:14:06 -0400

    back to dotdict

--- data/nuscenes.py
+++ data/nuscenes.py
@@ -5,7 +5,7 @@ import numpy as np
 from kornia.geometry.liegroup import Se3
 from torch.utils.data import Dataset
 
-from utils import geometry
+from utils import dotdict, geometry
 from utils.torch import numpy_to_torch
 
 
@@ -18,16 +18,16 @@ class Dataloader(Dataset):
 
     def __getitem__(self, index):
         with open(self.files[index], "rb") as f:
-            ex = numpy_to_torch(pickle.load(f))
+            ex = dotdict.dict_to_namespace(numpy_to_torch(pickle.load(f)))
 
-        not_ground1 = ex["pcl_t0"][:, 2] > 0.3
-        not_ground2 = ex["pcl_t1"][:, 2] > 0.3
+        not_ground1 = ex.pcl_t0[:, 2] > 0.3
+        not_ground2 = ex.pcl_t1[:, 2] > 0.3
 
-        pcl_1 = geometry.ego_to_sensor(ex["pcl_t0"][:, :3], ex["sensor"])
-        flow = geometry.ego_to_sensor(ex["pcl_t0"][:, :3] + ex["flow_t0_t1"], ex["sensor"]) - geometry.ego_to_sensor(
-            ex["pcl_t0[:, :3]"], ex["sensor"]
+        pcl_1 = geometry.ego_to_sensor(ex.pcl_t0[:, :3], ex.sensor)
+        flow = geometry.ego_to_sensor(ex.pcl_t0[:, :3] + ex.flow_t0_t1, ex.sensor) - geometry.ego_to_sensor(
+            ex.pcl_t0[:, :3], ex.sensor
         )
-        pcl_2 = geometry.ego_to_sensor(ex["pcl_t1"][:, :3], ex["sensor"])
+        pcl_2 = geometry.ego_to_sensor(ex.pcl_t1[:, :3], ex.sensor)
 
         _, m1 = geometry.filter_range(pcl_1, return_mask=True)
         m1 = m1 & not_ground1
@@ -38,9 +38,9 @@ class Dataloader(Dataset):
         m2 = m2 & not_ground2
         pcl_2 = pcl_2[m2]
 
-        annotations = ex["annotation_labels"][m1]
+        annotations = ex.annotation_labels[m1]
 
-        ego1_SE3_ego0 = Se3(ex["odom_t0_t1"][:3, :3], ex["odom_t0_t1"][:3, 3])
+        ego1_SE3_ego0 = Se3(ex.odom_t0_t1[:3, :3], ex.odom_t0_t1[:3, 3])
 
         return {
             "pcl_0": pcl_1,
